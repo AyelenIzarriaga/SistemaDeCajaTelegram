1f9461d12bf10ce74de41ffb477ea9d70ef0bedd
AyelenIzarriaga <ayeiza4133@gmail.com>
2026-03-05 18:45:48 -0300

    Improve health endpoint to check DB connection
    Enhance health check to include database connection status.

--- SistemaDeCaja-main/src/main/java/sistemacaja/com/HealthController.java
+++ SistemaDeCaja-main/src/main/java/sistemacaja/com/HealthController.java
@@ -1,14 +1,30 @@
 package sistemacaja.com;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @RestController
 public class HealthController {
 
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("/health")
     public String health() {
-        return "BOT_OK";
-    }
+        try (Connection conn = dataSource.getConnection()) {
 
+            if (conn.isValid(2)) {
+                return "BOT_OK | DB_OK";
+            } else {
+                return "BOT_OK | DB_FAIL";
+            }
+
+        } catch (Exception e) {
+            return "BOT_OK | DB_ERROR";
+        }
+    }
 }
