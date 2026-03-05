bdad30b2abda8de4664b7ac2cc634ea15a36e005
AyelenIzarriaga <ayeiza4133@gmail.com>
2026-03-05 18:29:09 -0300

    Create PingController.java

---
+++ SistemaDeCaja-main/src/main/java/sistemacaja/com/PingController.java
@@ -0,0 +1,14 @@
+package sistemacaja.com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/")
+    public String ping() {
+        return "ok";
+    }
+
+}
