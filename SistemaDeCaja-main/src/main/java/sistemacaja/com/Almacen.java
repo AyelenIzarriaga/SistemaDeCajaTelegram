0952b0c0115d11bd9e05529f4ea9cc520f0e4556
AyelenIzarriaga <ayeiza4133@gmail.com>
2026-03-10 18:18:40 -0300

    Refactor Almacen class with additional imports

--- SistemaDeCaja-main/src/main/java/sistemacaja/com/Almacen.java
+++ SistemaDeCaja-main/src/main/java/sistemacaja/com/Almacen.java
@@ -4,6 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 
 @Entity
@@ -54,4 +67,5 @@ public class Almacen {
 		this.Usuarios=Usuarios;
 	}
 }
+
 
