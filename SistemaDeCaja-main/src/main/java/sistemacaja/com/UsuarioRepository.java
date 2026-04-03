846381021248357dd9c146b32542bdc2f101de41
AyelenIzarriaga <ayeiza4133@gmail.com>
2026-04-03 18:22:44 -0300

    Remove unnecessary whitespace in UsuarioRepository

--- SistemaDeCaja-main/src/main/java/sistemacaja/com/UsuarioRepository.java
+++ SistemaDeCaja-main/src/main/java/sistemacaja/com/UsuarioRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByTelegramId(Long telegramId);
-
 }
